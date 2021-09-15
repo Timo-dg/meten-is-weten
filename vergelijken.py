288e184691ae658631923a51bc8f43c57eb4b953
Timo de Gruiter <timo@EDGITNEW.local>
2021-09-15 09:16:29 +0200

    elif-statement

--- vergelijken.py
+++ vergelijken.py
@@ -4,11 +4,12 @@ b = 200
 if a >   b:
     print('a is het grootste getal ')
 
-
-
-
 elif a < b:
     print('a is het kleinste getal')
+
+else:
+    a == b
+    print('a en b zijn even groot')
 
 
 
