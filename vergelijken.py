54f25cd5f460410656811fb556b7af9fdf31182d
Timo de Gruiter <timo@EDGITNEW.local>
2021-09-15 09:21:50 +0200

    Min en Max

--- vergelijken.py
+++ vergelijken.py
@@ -1,6 +1,9 @@
 a = 100
 b = 200
 
+a = 'Min'
+b = 'Max'
+
 if a >   b:
     print('a is het grootste getal ')
 
@@ -10,6 +13,9 @@ elif a < b:
 else:
     a == b
     print('a en b zijn even groot')
+
+print('Het minimum is: 100') 
+print('Het maximum is: 200') 
 
 
 
